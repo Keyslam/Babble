977c8c6cc5f8fb9a1fd431909dc6e7eb5d65a8b0
Pablo Ariel Mayobre (Positive07) <pablo.a.mayobre@gmail.com>
2017-12-05 18:25:44 -0300

    Check for isCallable in Link and Script components
    The function can probably be moved to other file and reused.
    
    Currently used in dialogue.lua, components/script.lua and components/link.lua

--- babble/src/components/link.lua
+++ babble/src/components/link.lua
@@ -2,19 +2,29 @@ local SubPath = (...):gsub('%.[^%.]+%.[^%.]+$', '')
 
 local Class = require(SubPath..".class")
 
+local function isCallable (f)
+  if type(f) == 'function' then
+    return true
+  elseif type(f) == 'table' then
+    local mt = getmetatable(f)
+    if mt and type(mt.__call) == 'function' then
+      return true
+    end
+  end
+
+  return false
+end
+
 local Link = Class()
 function Link:init(node, func, ...)
    self.node = node
-   local mt = getmetatable(func)
-   if type(func) == "function" or mt and type(mt.__call) == "function" then
+   if isCallable(func) then
       self.func = func
       self.args = {..., n = select("#", ...)}
    elseif type(func) == "string" then
       self.link = func
-   --[[
    else
-      error("bad argument #1 to 'link' (function expected)", 3)
-   ]]
+      error("bad argument #1 to 'link' (function or string expected)", 3)
    end
 end
 
